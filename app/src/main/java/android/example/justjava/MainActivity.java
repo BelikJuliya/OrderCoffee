72e8c65950bb0cce0966b9ca5eece3c89179f0dd
«Yuliya <Yuliya.Belik@harman.com>
2021-03-01 14:35:17 +0300

    Buttons design was changed

--- app/src/main/java/android/example/justjava/MainActivity.java
+++ app/src/main/java/android/example/justjava/MainActivity.java
@@ -1,13 +1,4 @@
-/**
- * IMPORTANT: Make sure you are using the correct package name.
- * This example uses the package name:
- * package com.example.android.justjava
- * If you get an error when copying this code into Android studio, update it to match teh package name found
- * in the project's AndroidManifest.xml file.
- **/
-
 package android.example.justjava;
-
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,20 +15,18 @@ import androidx.appcompat.app.AppCompatActivity;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    private CheckBox toppingCheckBox;
-    private CheckBox chocolateCheckBox;
-    boolean isWhippedCream;
-    boolean isChocolate;
-    private int quantity = 0;
-    private int price;
+    private boolean isWhippedCream;
+    private boolean isChocolate;
+    private int mQuantity = 0;
+    private int mPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toppingCheckBox = findViewById(R.id.topping_checkbox);
-        chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        CheckBox toppingCheckBox = findViewById(R.id.topping_checkbox);
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
         isWhippedCream = toppingCheckBox.isChecked();
         isChocolate = chocolateCheckBox.isChecked();
     }
@@ -46,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        calculatePrice(quantity);
-        sendEmail(createOrderSummary(price));
+        calculatePrice(mQuantity);
+        sendEmail(createOrderSummary(mPrice));
     }
 
     /**
@@ -55,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void display(int number) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(number);
     }
 
     /**
@@ -67,20 +56,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
-        if (quantity <100) {
-            display(++quantity);
-        } else{
-            Toast toast = Toast.makeText(this, getString(R.string.too_many_cups), Toast.LENGTH_LONG );
+        if (mQuantity < 100) {
+            display(++mQuantity);
+        } else {
+            Toast toast = Toast.makeText(this, getString(R.string.too_many_cups), Toast.LENGTH_LONG);
             toast.show();
         }
     }
 
-
     public void decrement(View view) {
-        if (quantity > 0) {
-            display(--quantity);
-        } else{
-            Toast toast = Toast.makeText(this, getString(R.string.too_few_cups), Toast.LENGTH_SHORT );
+        if (mQuantity > 0) {
+            display(--mQuantity);
+        } else {
+            Toast toast = Toast.makeText(this, getString(R.string.too_few_cups), Toast.LENGTH_SHORT);
             toast.show();
         }
     }
@@ -91,27 +79,27 @@ public class MainActivity extends AppCompatActivity {
         CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
         boolean isChocolate = chocolateCheckBox.isChecked();
 
-        price = quantity * 5;
+        mPrice = quantity * 5;
         if (isWhippedCream) {
-            price++;
+            mPrice++;
         }
         if (isChocolate) {
-            price += 2;
+            mPrice += 2;
         }
     }
 
     private String createOrderSummary(int price) {
         TextView nameTextView = findViewById(R.id.enter_name_edit_text);
         String name = nameTextView.getText().toString();
-        return "Name: " + name + "\nAdd whipped cream? " + isWhippedCream + "\nAdd Chocolate? " + isChocolate + "\nQuantity: " + quantity + "\n" + "Total: $" + price + "\n" + "Thank you!";
+        return "Name: " + name + "\nAdd whipped cream? " + isWhippedCream + "\nAdd Chocolate? " + isChocolate + "\nQuantity: " + mQuantity + "\n" + "Total: $" + price + "\n" + "Thank you!";
     }
 
-    private void sendEmail (String orderSummary){
+    private void sendEmail(String orderSummary) {
         Intent sendIntent = new Intent(Intent.ACTION_SEND);
         sendIntent.setType("*/*");
         sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Order");
         sendIntent.putExtra(Intent.EXTRA_TEXT, orderSummary);
-        if (sendIntent.resolveActivity(getPackageManager()) != null){
+        if (sendIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(sendIntent);
         }
     }
